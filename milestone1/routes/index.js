e5b07812b5e9cd5baa98fb23f2496066eb3a9b5e
Julia de Veyra <julianne_deveyra@dlsu.edu.ph>
2024-03-15 22:35:37 +0800

    frontend for announcements admin-side

--- milestone1/routes/index.js
+++ milestone1/routes/index.js
@@ -295,14 +295,24 @@ router.get("/admin/logout", (req, res, next) => {
 
 // render admin dashboard
 router.get('/admin/dashboard', checkNotAuthenticatedAdmin, async (req, res)=>{
-    const getObjectParams = {
-        Bucket: bucketName,
-        Key: req.user.profilepic, // file name
-    }
-    const command = new GetObjectCommand(getObjectParams);
-    const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+    const posts = [
+        { date: '03/03/2023', admin: 'admin1', title: 'Test1', announcement: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis dignissim odio at cursus. Aliquam erat volutpat. Etiam ligula dui, ultricies vitae bibendum sit amet, dignissim et justo. Aenean tempus, arcu sit amet eleifend fringilla, est lacus ullamcorper magna, sit amet sagittis odio lacus sit amet nibh. Cras magna tortor, pharetra quis urna at, consectetur eleifend massa. Morbi hendrerit enim eu hendrerit viverra. Fusce ac eros leo. Duis at sollicitudin orci.' },
+        { date: '03/03/2023', admin: 'admin2', title: 'Test2', announcement: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis dignissim odio at cursus. Aliquam erat volutpat. Etiam ligula dui, ultricies vitae bibendum sit amet, dignissim et justo. Aenean tempus, arcu sit amet eleifend fringilla, est lacus ullamcorper magna, sit amet sagittis odio lacus sit amet nibh. Cras magna tortor, pharetra quis urna at, consectetur eleifend massa. Morbi hendrerit enim eu hendrerit viverra. Fusce ac eros leo. Duis at sollicitudin orci.' },
+        { date: '03/03/2023', admin: 'admin3', title: 'Test3', announcement: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis dignissim odio at cursus. Aliquam erat volutpat. Etiam ligula dui, ultricies vitae bibendum sit amet, dignissim et justo. Aenean tempus, arcu sit amet eleifend fringilla, est lacus ullamcorper magna, sit amet sagittis odio lacus sit amet nibh. Cras magna tortor, pharetra quis urna at, consectetur eleifend massa. Morbi hendrerit enim eu hendrerit viverra. Fusce ac eros leo. Duis at sollicitudin orci.' }
+    ]
 
-    return res.render('bookAdmin', { user: req.user.username, userpic: url });
+    const data = {
+        user: req.user.username,
+        posts
+    }
+
+    return res.render('adminDashboard', data);
+});
+
+// create announcement
+router.post('/admin/create-announcement', checkNotAuthenticatedAdmin, async (req, res)=>{
+    console.log(req.body);
+    return res.redirect('/admin/dashboard');
 });
 
 
