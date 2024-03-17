8d16cf0189b0666aa3fdce25c698fb8a0d529d83
Julia de Veyra <julianne_deveyra@dlsu.edu.ph>
2024-03-17 22:13:06 +0800

    partially working scholar profiles and scholarship renewal

--- milestone1/controllers/userProfileController.js
+++ milestone1/controllers/userProfileController.js
@@ -32,6 +32,8 @@ app.use(express.urlencoded({
   extended: true
 }));
 
+let person = {};
+
 // ======= METHODS ======= //
 const controller = {
 
@@ -68,7 +70,7 @@ const controller = {
                         console.log('Incomplete registration');
                         res.status(500).send('Internal Server Error');
                     } else {
-                        const person = { 
+                        person = { 
                             userpic: url, 
                             scholar: (req.user.firstname).concat(" ", req.user.lastname), 
                             email: req.user.email, 
@@ -90,21 +92,182 @@ const controller = {
 
     // UPDATE PROFILE PICTURE
     updateProfilePicture: async (req, res)=>{
-        console.log(req.body);
-        console.log(req.file);
-        return res.redirect('/users/profile');
+        const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
+        const file = req.file;
+        let errors = [];
+        console.log(file);
+
+        // ======= FILE VALIDATION ======= //
+        // size check
+        const maxSize = 1024 * 1024 * 1; // 1 for 1mb
+        if (req.file.size > maxSize ){
+            errors.push({ message: "Max upload size 1MB." });
+        }
+
+        // extention based file type check
+        if (req.file.mimetype != "image/jpeg" && req.file.mimetype != "image/jpg" && req.file.mimetype != "image/png"){
+            errors.push({ message: "File is not a .PNG .JPEG or .JPG file." });
+        }
+
+        // file siggy based file type check
+        buffer = req.file.buffer
+        const magicNum = buffer.toString('hex', 0, 4);
+        const pngNum = "89504e47"
+        const jpegNum = "ffd8ffe0"
+        const riffNum = "52494646"
+        if (magicNum !== pngNum && magicNum !== jpegNum && magicNum !== riffNum){
+            errors.push({ message: ".PNG .JPEG or .JPG files only." });
+        }
+
+        // resize image
+        const fileBuffer = await sharp(req.file.buffer)
+            .resize({height: 180, width: 180, fit: "contain" })
+            .toBuffer();
+        
+        // config the upload details to send to s3
+        const fileName = generateFileName();
+        const uploadParams = {
+            Bucket: bucketName,
+            Body: fileBuffer,  // actual image data
+            Key: fileName, // becomes the file name
+            ContentType: file.mimetype
+        };
+
+        if (errors.length > 0){
+            person.errors = errors;
+            res.render('userProfile', person);
+        } else {
+            // send data to s3 bucket
+            await s3.send(new PutObjectCommand(uploadParams));
+
+            pool.query(
+                `UPDATE users
+                SET profilepic = $1
+                WHERE email = $2
+                RETURNING *`, [fileName, req.user.email], (err, results)=>{
+                    if (err) {
+                        console.error('Error: ', err);
+                        res.status(500).send('Internal Server Error');
+                    } else {
+                        console.log(results.rows);
+                        return res.redirect('/users/profile');
+                    }
+                }
+            );
+        }
     },
 
     // UPDATE PROFILE INFORMATION
     updateProfileInformation: async (req, res)=>{
+        let { univ, degree } = req.body;
+        let errors = [];
         console.log(req.body);
-        return res.redirect('/users/profile');
+
+        // ======= INPUT VALIDATION ======= //
+        // check that there are no empty inputs
+        if (!univ || !degree) {
+            errors.push({ message: "Please enter all fields." });
+        }
+
+        const regex = /^[ a-zA-Z0-9.,!?&%#@:;*\-+=]{1,64}$/;
+        if (!(regex.test(univ))){
+            errors.push({ message: "Invalid university input" });
+        }
+        if (!(regex.test(degree))){
+            errors.push({ message: "Invalid degree input" });
+        }
+
+        // ======= PSQL ======= //
+        if (errors.length > 0){
+            person.errors = errors;
+            res.render('userProfile', person);
+        } else {
+            pool.query(
+                `UPDATE users_additional_info
+                SET university = $1, degree = $2
+                WHERE email = $3
+                RETURNING *`, [univ, degree, req.user.email], (err, results)=>{
+                    if (err) {
+                        console.error('Error: ', err);
+                        res.status(500).send('Internal Server Error');
+                    } else {
+                        console.log(results.rows);
+                        return res.redirect('/users/profile');
+                    }
+                }
+            );
+        }
     },
 
     // COMPLETE PROFILE INFORMATION
     completeProfileInformation: async (req, res)=>{
+        let { scholartype, univ, degree, accName, accNum } = req.body;
+        let errors = [];
         console.log(req.body);
-        return res.redirect('/users/profile');
+
+        // ======= INPUT VALIDATION ======= //
+        // check that there are no empty inputs
+        if (!scholartype || !univ || !degree || !accName || !accNum ) {
+            errors.push({ message: "Please enter all fields." });
+        }
+
+        if (scholartype != "Merit" && scholartype != "RA 7687"){
+            errors.push({ message: "Incorrect Scholar Type" });
+        }
+
+        const regex = /^[ a-zA-Z0-9.,!?&%#@:;*\-+=]{1,64}$/;
+        if (!(regex.test(univ))){
+            errors.push({ message: "Invalid university input" });
+        }
+        if (!(regex.test(degree))){
+            errors.push({ message: "Invalid degree input" });
+        }
+        if (!(regex.test(accName))){
+            errors.push({ message: "Invalid account name input" });
+        }
+
+        const accnumregex = /^\d{10}$/;
+        if (!(accnumregex.test(accNum))){
+            errors.push({ message: "Invalid account number input" });
+        }
+
+        // ======= PSQL ======= //
+        if (errors.length > 0){
+            person.errors = errors;
+            res.render('userProfile', person);
+        } else {
+            pool.query(
+                `SELECT account_num
+                FROM users_additional_info
+                WHERE account_num = $1`, [accNum], (err, results)=>{
+                    if (err) {
+                        console.error('Error: ', err);
+                        res.status(500).send('Internal Server Error');
+                    } else {
+                        if (results.rows.length > 0){
+                            errors.push({ message: "Account number already registered." });
+                            person.errors = errors;
+                            res.render('userProfile', person);
+                        } else { // complete the information
+                            pool.query(
+                                `UPDATE users_additional_info
+                                SET scholar_type = $1, university = $2, degree = $3, account_name = $4, account_num = $5
+                                WHERE email = $6
+                                RETURNING *`, [scholartype, univ, degree, accName, accNum, req.user.email], (err, results)=>{
+                                    if (err) {
+                                        console.error('Error: ', err);
+                                        res.status(500).send('Internal Server Error');
+                                    } else {
+                                        console.log(results.rows);
+                                        return res.redirect('/users/profile');
+                                    }
+                                }
+                            );
+                        }
+                    }
+                }
+            );
+        }
     }
 
 };
--- milestone1/controllers/userRenewController.js
+++ milestone1/controllers/userRenewController.js
@@ -37,36 +37,48 @@ const controller = {
 
     // LOAD RENEWAL APPLICATIONS
     getRenewalApps: async (req, res)=>{
-        const getEAFParams = {
-            Bucket: bucketName,
-            Key: 'Thesis-Allowance-Guidelines-1.pdf', // [TODO]: sample only
-        }
-        const eaf_command = new GetObjectCommand(getEAFParams);
-        const eaf_url = await getSignedUrl(s3, eaf_command, { expiresIn: 3600 });
-    
-        const getGradesParams = {
-            Bucket: bucketName,
-            Key: 'Thesis-Allowance-Guidelines-1.pdf', // [TODO]: sample only
-        }
-        const grades_command = new GetObjectCommand(getGradesParams);
-        const grades_url = await getSignedUrl(s3, grades_command, { expiresIn: 3600 });
+        try {
+            const results = await pool.query(
+                `SELECT *, TO_CHAR(date_submitted, 'YYYY-MM-DD') AS date_submitted FROM scholar_renewal_applications WHERE status != $1`, ['Deleted']);
+            
+            const applications = results.rows;
 
-        const getApproveParams = {
-            Bucket: bucketName,
-            Key: 'Thesis-Allowance-Guidelines-1.pdf', // [TODO]: sample only
+            for (let i = 0; i < applications.length; i++){
+                if (applications[i].eaf != null && applications[i].grades != null){
+                    const getEAFParams = {
+                        Bucket: bucketName,
+                        Key: applications[i].eaf,
+                    }
+                    const eaf_command = new GetObjectCommand(getEAFParams);
+                    const eaf_url = await getSignedUrl(s3, eaf_command, { expiresIn: 3600 });
+                    applications[i].eaf = eaf_url
+    
+                    const getGradesParams = {
+                        Bucket: bucketName,
+                        Key: applications[i].grades,
+                    }
+                    const grades_command = new GetObjectCommand(getGradesParams);
+                    const grades_url = await getSignedUrl(s3, grades_command, { expiresIn: 3600 });
+                    applications[i].grades = grades_url
+                }
+        
+                if (applications[i].approve_document != null){
+                    const getApproveParams = {
+                        Bucket: bucketName,
+                        Key: applications[i].approve_document, // [TODO]: sample only
+                    }
+                    const approve_command = new GetObjectCommand(getApproveParams);
+                    const approve_url = await getSignedUrl(s3, approve_command, { expiresIn: 3600 });
+                    applications[i].approve_document = approve_url
+                }
+            }
+            
+            console.log(applications);
+            return res.render('userRenew', { applications });
+        } catch (error) {
+            console.error('Error fetching applications: ', error);
+            return res.status(500).send('Internal Server Error');
         }
-        const approve_command = new GetObjectCommand(getApproveParams);
-        const approve_url = await getSignedUrl(s3, approve_command, { expiresIn: 3600 });
-    
-        // sample data only, replace when querying db
-        // id here is for application NOT user
-        const applications = [
-            { id: 1, eaf: eaf_url, grades: grades_url, approved: approve_url, date: '03/03/2024', status: 'Approved' },
-            { id: 2, eaf: eaf_url, grades: grades_url, approved: null, date: '03/04/2024', status: 'Rejected' },
-            { id: 3, eaf: eaf_url, grades: grades_url, approved: null, date: '03/05/2024', status: 'Pending' }
-        ]
-    
-        return res.render('userRenew', { applications });
     },
 
     // DELETE PENDING RENEWAL APPLICATION
@@ -77,8 +89,84 @@ const controller = {
 
     // APPLY FOR RENEWAL
     applyRenewal: async (req, res)=>{
-        console.log(req.body);
-        console.log(req.files);
+        const today = new Date();
+        const day = today.getDate();
+        const month = today.getMonth() + 1; 
+        const year = today.getFullYear();
+        date = year +"-"+ month +"-"+ day;
+
+        const generateFileName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
+        const eaf_file = req.files.eaf[0];
+        const grades_file = req.files.grades[0];
+        console.log('file1: ', req.files.eaf[0]);
+        console.log('file1: ', req.files.grades[0]);
+        let errors = [];
+
+        // ======= FILE VALIDATION ======= //
+        // size check
+        const maxSize = 1024 * 1024 * 1; // 1 for 1mb
+        if (eaf_file.size > maxSize && grades_file.size > maxSize){
+            errors.push({ message: "Max upload size 1MB." });
+        }
+
+        console.log('test: ', req.files)
+
+        // extention based file type check
+        if (eaf_file.mimetype != "application/pdf" && grades_file.mimetype != "application/pdf"){
+            errors.push({ message: "Files are not a .PDF file." });
+        }
+
+        // file siggy based file type check
+        eaf_buffer = req.files.eaf[0].buffer;
+        grades_buffer = req.files.grades[0].buffer;
+        const eaf_magicNum = eaf_buffer.toString('hex', 0, 4);
+        const grades_magicNum = grades_buffer.toString('hex', 0, 4);
+        const pdfNum = "25504446";
+        if (eaf_magicNum !== pdfNum && grades_magicNum !== pdfNum){
+            errors.push({ message: ".PDF files only." });
+        }
+
+        // resize image
+        const eafBuffer = await sharp(req.files.eaf[0].buffer)
+            .resize({height: 180, width: 180, fit: "contain" })
+            .toBuffer();
+        const gradesBuffer = await sharp(req.files.grades[0].buffer)
+            .resize({height: 180, width: 180, fit: "contain" })
+            .toBuffer();
+        
+        // config the upload details to send to s3
+        const eafName = generateFileName();
+        const gradesName = generateFileName();
+
+        const getEAFParams = {
+            Bucket: bucketName,
+            Body: eafBuffer,  // actual eaf data
+            Key: eafName, // becomes the file name
+            ContentType: req.files.eaf[0].mimetype
+        };
+
+        const getGradesParams = {
+            Bucket: bucketName,
+            Body: gradesBuffer,  // actual grades data
+            Key: gradesName, // becomes the file name
+            ContentType: req.files.grades[0].mimetype
+        };
+
+        // pool.query(
+        //     `UPDATE users
+        //     SET profilepic = $1
+        //     WHERE email = $2
+        //     RETURNING *`, [fileName, req.user.email], (err, results)=>{
+        //         if (err) {
+        //             console.error('Error: ', err);
+        //             res.status(500).send('Internal Server Error');
+        //         } else {
+        //             console.log(results.rows);
+        //             return res.redirect('/users/profile');
+        //         }
+        //     }
+        // );
+
         return res.redirect('/users/renew-scholarship');
     }
 
